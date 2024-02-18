0c2e7a207123078b9f406e50817db4680e25f3c4
animeshanand1 <animeshanand9@gmail.com>
2024-02-18 13:59:15 +0530

    fixed image gallery function

--- client/src/Components/SpecificProduct/SpecificProduct.js
+++ client/src/Components/SpecificProduct/SpecificProduct.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './specificProduct.module.css'
 function SpecificProduct() {
+    const [mainImageSrc, setMainImageSrc] = useState("https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8fHww");
+
+    const handleThumbnailClick = (event) => {
+        const thumbnailSrc = event.target.getAttribute('src');
+        setMainImageSrc(thumbnailSrc);
+        event.target.setAttribute('src', mainImageSrc);
+    };
     return (
-        <main classNameName={styles["product-page"]}>
+        <main className={styles["product-page"]}>
             <div className={styles["image-gallery"]}>
                 <div className={styles["thumbnails"]}>
-                    <img src="https://images.unsplash.com/photo-1628102160424-5f4ab3404829?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGZhc2hpb24lMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3Dg" alt="Thumbnail 1" className={styles["thumbnail"]} data-fullimage="image1.jpg" />
-                    <img src="https://images.unsplash.com/photo-1627292441194-0280c19e74e4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZhc2hpb24lMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3Dg" alt="Thumbnail 2" className={styles["thumbnail"]} data-fullimage="image2.jpg" />
-                    <img src="https://images.unsplash.com/photo-1619785292559-a15caa28bde6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8fHww" alt="Thumbnail 3" className={styles["thumbnail"]} data-fullimage="image3.jpg" />
+                    <img onClick={handleThumbnailClick} src="https://images.unsplash.com/photo-1628102160424-5f4ab3404829?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGZhc2hpb24lMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3Dg" alt="Thumbnail 1" className={styles["thumbnail"]} data-fullimage="image1.jpg" />
+                    <img onClick={handleThumbnailClick} src="https://images.unsplash.com/photo-1627292441194-0280c19e74e4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZhc2hpb24lMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3Dg" alt="Thumbnail 2" className={styles["thumbnail"]} data-fullimage="image2.jpg" />
+                    <img onClick={handleThumbnailClick} src="https://images.unsplash.com/photo-1619785292559-a15caa28bde6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8fHww" alt="Thumbnail 3" className={styles["thumbnail"]} data-fullimage="image3.jpg" />
                 </div>
                 <div className={styles["main-image"]}>
-                    <img src="https://images.unsplash.com/photo-1613915617430-8ab0fd7c6baf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8fHww" alt="Main Image" className={styles["full-image"]} />
+                    <img
+                        src={mainImageSrc}
+                        alt="Main Image"
+                        className={styles["full-image"]}
+                    />
                 </div>
             </div>
 
