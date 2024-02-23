331820d0a3d4687d7dabd7cc0402b0a6f88f39d1
animeshanand1 <animeshanand9@gmail.com>
2024-02-23 22:40:38 +0530

    improved css otp page

--- client/src/Components/OTP/Otp.js
+++ client/src/Components/OTP/Otp.js
@@ -3,18 +3,17 @@ import styles from './otp.module.css'
 
 function Otp() {
     return (
-
         <div className={styles.container}>
             <h4>Enter OTP Code</h4>
-            <form action="#">
+            <form>
                 <div className={styles["input-field"]}>
                     <input type="number" className={styles["otp-input"]} />
                     <input type="number" className={styles["otp-input"]} disabled />
                     <input type="number" className={styles["otp-input"]} disabled />
                     <input type="number" className={styles["otp-input"]} disabled />
                 </div>
-                <button className={styles["verify-btn"]}>Verify OTP</button>
             </form>
+            <button className={styles["verify-btn"]}>Verify OTP</button>
         </div>
 
     )
