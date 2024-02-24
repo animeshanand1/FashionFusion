3a0d4d1ddb6d340c0ebc1be363f59002b24f98de
animeshanand1 <animeshanand9@gmail.com>
2024-02-24 22:18:01 +0530

    update existing user's data

--- server/user/controllers/authController.js
+++ server/user/controllers/authController.js
@@ -14,7 +14,7 @@ const userLogin = async (req, res) => {
         }
         const matchedPassword=await bcrypt.compare(password,isUser.password)
         if(matchedPassword){
-            const token = jwt.sign({ userId: isUser._id }, 'asaas', { expiresIn: '2h' });
+            const token = jwt.sign({ userId: isUser._id }, secretKey, { expiresIn: '1h' });
             return res.status(200).json({ message: 'Signed In',token })
         }
         return res.status(401).json({ message: 'Bad Credentials' });
@@ -41,4 +41,22 @@ const userSignup = async (req, res) => {
         res.status(500).json({ message: 'Something went wrong' })
     }
 }
-module.exports = { userLogin, userSignup }
+
+const updateUser=async(req,res)=>{
+    try {
+        const {id}=req.params
+        const {name}=req.body
+        const user=await User.findById(id)
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        
+        user.name =name;
+        await user.save();
+        return res.status(201).json({ message: 'User updated successfully', User });
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Something went wrong' })
+    }
+}
+module.exports = { userLogin, userSignup,updateUser }
--- server/user/router/routes.js
+++ server/user/router/routes.js
@@ -1,8 +1,9 @@
 
 const express=require('express');
-const { userLogin, userSignup } = require('../controllers/authController');
+const { userLogin, userSignup, updateUser } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/login',userLogin)
 router.post('/register',userSignup)
+router.post('/updateAccount/:id',updateUser)
 module.exports=router
