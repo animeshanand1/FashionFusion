d58cb7a42d30378e9188adfa87fa327f28b01f87
animeshanand1 <animeshanand9@gmail.com>
2024-02-25 21:01:37 +0530

    css optimised

--- client/src/Pages/Cart/CartCard.js
+++ client/src/Pages/Cart/CartCard.js
@@ -3,43 +3,34 @@ import styles from './cartCard.module.css'
 
 function CartCard() {
   return (
-    <div className={styles.column}>
-      <div className={styles["div-3"]}>
-        <div className={styles["div-4"]}>
-          <img
-            loading="lazy"
-            className={styles.img}
-          />
-          <div className={styles["div-5"]}>Shopping Continue</div>
+    <div className={styles.savedCart}>
+      <div className={styles[""]}>
+        <div className={styles[""]}>
+          <div className={styles["continue"]}>Shopping Continue</div>
         </div>
-        <div className={styles["div-6"]}>
-          <div className={styles["div-8"]}>Shopping cart</div>
-          <div className={styles["div-9"]}>You have 3 item in your cart</div>
-          <div className={styles["div-10"]}>
-            <div className={styles["div-11"]}>
-              <img
-                loading="lazy"
-                className={styles["img-2"]}
-              />
-              <div className={styles["div-12"]}>
-                <div className={styles["div-13"]}>Italy Pizza</div>
-                <div className={styles["div-14"]}>Extra cheese and toping</div>
-              </div>
+        <div className={styles["item-number"]}>You have 3 item in your cart</div>
+        <div className={styles["cart-box"]}>
+          <div className={styles["box-deatils"]}>
+            <img
+              loading="lazy"
+              className={styles["box-item-img"]}
+            />
+            <div className={styles["box-item-description"]}>
+              <div className={styles["item-name"]}>Italy Pizza</div>
+              <div className={styles["box-item-addOn"]}>Extra cheese and toping</div>
             </div>
-            <div className={styles["div-15"]}>
-              <div className={styles["div-16"]}>1</div>
-              <img
-                loading="lazy"
-                className={styles["img-3"]}
-              />
-            </div>
-            <div className={styles["div-17"]}>
-              <div className={styles["div-18"]}>$681</div>
-              <img
-                loading="lazy"
-                className={styles["img-4"]}
-              />
-            </div>
+          </div>
+          <div className={styles["ordered-pcs-details"]}>
+            <div className={styles["ordered-pcs-num"]}>1</div>
+            <img
+              loading="lazy"
+            />
+          </div>
+          <div className={styles["ordered-summary"]}>
+            <div className={styles["div-price"]}>$681</div>
+            <img
+              loading="lazy"
+            />
           </div>
         </div>
       </div>
